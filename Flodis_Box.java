1f257e9b7249d7389246f7c4f9d51acf88c4aadd
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-07 23:47:25 +0530

    Add files via upload

---
+++ Flodis_Box.java
@@ -0,0 +1,16 @@
+class Flodis_Box
+{
+public static void main(String[] args)
+{
+ int n = 1;
+ for(int i=1; i<=5; i++)
+{
+  for(int j=1; j<=5; j++)
+{
+System.out.printf("%3d",n);
+n++;
+}
+System.out.println();
+}
+}
+}
